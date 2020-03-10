47afedd1d2537952e4c47db6bb8ad0597a2f7f03
Nghia.Lam <nghialamngocit@gmail.com>
2020-03-10 22:20:08 +0700

    change config

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application} from 'express';
 
 var app : Application = express();
 app.get('/', (req, res) => {
-
+  res.send('Hello');
 })
 const POST: string = process.env.PORT || '5000';
 app.listen(POST, () => console.log(`Server running on port ${POST}`))
