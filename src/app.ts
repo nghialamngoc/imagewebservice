aff200b0e650151d8ceb6b0176b3c5bdd50bc1c2
Nghia.Lam <nghialamngocit@gmail.com>
2020-03-16 21:55:31 +0700

    implement upload image and log

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, { Application, Router } from 'express';
 
+const bodyParse = require('body-parser');
 const app : Application = express();
+
+//Middleware
+app.use(bodyParse({limit: '50mb'}));
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
 
 //Router import
 import ImageRouter from './routes/imageRouter';
--- src/routes/imageRouter.ts
+++ src/routes/imageRouter.ts
@@ -1,13 +1,59 @@
 import express, { Request, Response, NextFunction }  from 'express';
 
 let router = express.Router();
-let formidable = require('formidable');
 let fs = require('fs');
+let multer  = require('multer');
 
-router.get('/test', (req: Request, res: Response) => {
-  res.send('hello');
+// multer storage config
+const storage = multer.diskStorage({
+  destination: function( req: any, file: any, cb: ( err: any, dest: string ) => void ){
+    cb( null, './uploads' );
+  },
+  filename: function( req: any, file: any, cb: ( err: any, dest: string ) => void ){
+    cb( null, new Date().toISOString().slice(0, 10) + '_' + file.originalname );
+  }
+});
+const fileFilter = function (req: any, file: any, cb: ( err: any, next?: boolean ) => void) {
+  // const filetypes = /jpeg|jpg|png/;
+  // const isSupport: boolean = filetypes.test(file.originalname.slice(file.originalname.length - 4));
+  if ( file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ) {
+    cb(null, true);
+  } else {
+    cb(new Error(`File upload only supports the following filetypes -  ${file.mimetype}`));
+  }
+}
+// setup multer
+var upload = multer({ storage, fileFilter, limits: { fileSize: 1024 * 1024 * 10 } });
+var singleUpload = upload.single('image');
+var multiUpload = upload.array( 'images', 5 );
+// Support log
+var log4js = require('log4js');
+// log4js configure
+log4js.configure({
+  appenders: { image_router: { type: 'file', filename: 'image_router.log' } },
+  categories: { default: { appenders: ['image_router'], level: 'error' } }
+});
+
+var logger = log4js.getLogger('image_router'); 
+logger.level = 'debug';
+
+// GET LOG
+router.get('/image_upload/log', (req: Request, res: Response) => {
+  const URL: string = 'image_router.log' ;
+  fs.readFile( URL, 'utf8', ( err: any, data: any ) : void => {
+    if( err != null && err != undefined ){
+      res.status(404).json({
+        resultInfo: 'Nack',
+        result: {
+          message: `Cannot open log. Error is ${err}` 
+        }
+      });
+    } else {
+      res.writeHead( 200, {"Content-Type": "text/plain"});
+      res.end(data);
+    }
+  })
 })
-
 // OPEN IMAGE
 router.get('/open_image', (req: Request, res: Response) => {
   const URL: string = 'uploads/' + req.query.image_name;
@@ -26,58 +72,76 @@ router.get('/open_image', (req: Request, res: Response) => {
     }
   })
 })
-
-// UPLOAD IMAGE
-router.post('/upload_images', (req: Request ,res: Response, next: NextFunction ) => {
-  const options = {
-    uploadDir: 'uploads',
-    keepExtensions: true,
-    maxFileSize: 20 * 1024 * 1024, // 20MB
-    multiples: true
-  }
-  let form = formidable(options);
-  form.parse( req, ( err: any, fields: any, files: any ): void => {
-    if( err != null ){
+// UPLOAD SINGLE IMAGE
+router.post('/upload_image', ( req: { [index: string ] : any } ,res: Response, next: NextFunction ) => {
+  logger.debug('[Start] upload_image single file upload');
+  singleUpload( req, res, ( err: any ) => {
+    logger.debug('/upload_image file data: ', req.file);
+    if( err != null || err != undefined ){
+      logger.error('/upload_image error: ', err.toString());
       res.status(404).json({
         resultInfo: 'Nack',
         result: {
-          message: `Cannot upload image. Error is ${err}` 
+          message: err.toString()
         }
       });
-      return;
-    }
-    let file: any = files[""];
-    if( file.length > 0 ){
-      let fileNames: string[] = [];
-      file.forEach(( eachFile : { [index: string]:any }) : void => {
-        fileNames.push(eachFile["path"]);
+    } else if ( req.file == null || req.file == undefined ){
+      logger.error('/upload_image error: req.file == null || req.file == undefined');
+      res.status(404).json({
+        resultInfo: 'Nack',
+        result: {
+          message: "No image to upload"
+        }
       });
+    } else {
+      // Upload images successfully
+      logger.debug('/upload_image Upload image successfully');
       res.status(201).json({
         resultInfo: 'Ack',
         result: {
-          data: fileNames,
-          numberOfImages: fileNames.length,
-          message: "Upload images successfully"
-        }
-      })
-    } else if( file.type.includes('image') && file.size > 0 ){
-      res.status(201).json({
-        resultInfo: 'Ack',
-        result: {
-          data: file.path,
+          data: req.file.filename,
           numberOfImages: 1,
           message: "Upload images successfully"
         }
       })
-    } else {
+    }
+    
+  });
+})
+// UPLOAD MULTIPLE  IMAGE
+router.post('/upload_images', ( req: { [index: string ] : any } ,res: Response, next: NextFunction ) => {
+  logger.debug('[Start] upload_images multiple files upload');
+  multiUpload( req, res, ( err: any ) => {
+    logger.debug('/upload_images array file data: ', req.files);
+    if( err != null || err != undefined ){
+      logger.error('/upload_image error: ', err.toString());
       res.status(404).json({
         resultInfo: 'Nack',
         result: {
-          message: "No images to upload"
+          message: err.toString()
+        }
+      });
+    } else if ( req.files == null || req.files == undefined || req.files.length == 0 ){
+      logger.error('/upload_image error: req.files.length == 0');
+      res.status(404).json({
+        resultInfo: 'Nack',
+        result: {
+          message: "No image to upload"
+        }
+      });
+    } else {
+      // Upload images successfully
+      logger.debug('/upload_images Upload images successfully');
+      res.status(201).json({
+        resultInfo: 'Ack',
+        result: {
+          data: req.files.map((x: any) => x.filename ),
+          numberOfImages: req.files.length,
+          message: "Upload images successfully"
         }
       })
     }
-  })
-});
+  });
+})
 
 export default router;
